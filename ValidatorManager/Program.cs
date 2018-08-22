84ba438934ad429754e363d79782f337e4670344
ronsun <ron.sun@hotmail.com.tw>
2018-08-23 00:55:05 +0800

    add tests and better demo

--- ValidatorManager/Program.cs
+++ ValidatorManager/Program.cs
@@ -9,69 +9,56 @@ namespace ValidatorManager
     class Program
     {
         private static decimal validAmount = 11;
-        private static decimal invalidAmount = 10;
-
         private static string validEmail = "ron.sun@mailserver.com";
-        private static string invalidEmail = "a";
-
         private static string validCountry = "Taiwan";
-        private static string invalidCountry = "a";
-
         private static string validFoo = "Foo";
-        private static string inValidFoo = "a";
-
         private static string validBar = "Bar";
-        private static string inValidBar = "a";
 
         static void Main(string[] args)
         {
-            BasicDemo();
-            SimpleDemo();
-            StandardDemo();
-            AdvanceDemo();
-            ComplexDemo();
+            // Test
+            var tests = new Tests();
+            tests.BasicTest();
+            tests.SimpleTest();
+            tests.AdvanceTest();
+            tests.ComplexTest();
+
+            // Demo
+            //BasicDemo();
+            //SimpleDemo();
+            //StandardDemo();
+            //AdvanceDemo();
+            //ComplexDemo();
+
             Console.ReadLine();
         }
 
         static void BasicDemo()
         {
             var manager = new Basic.ValidatorManager();
+            bool isValid;
 
-            Console.WriteLine($"Basic.ValidatorManager.BasicValidation() => ");
-            Console.WriteLine($"isValid: {manager.BasicValidation(invalidAmount, validEmail)}");
-            Console.WriteLine($"isValid: {manager.BasicValidation(validAmount, invalidEmail)}");
-            Console.WriteLine($"isValid: {manager.BasicValidation(validAmount, validEmail)}");
+            isValid = manager.BasicValidation(validAmount, validEmail);
+            Console.WriteLine($"Basic.ValidatorManager.BasicValidation() => {isValid}");
 
-            Console.WriteLine();
-
-            Console.WriteLine($"Basic.ValidatorManager.FullValidation() => ");
-            Console.WriteLine($"isValid: {manager.FullValidation(invalidAmount, validEmail, validCountry)}");
-            Console.WriteLine($"isValid: {manager.FullValidation(validAmount, invalidEmail, validCountry)}");
-            Console.WriteLine($"isValid: {manager.FullValidation(validAmount, validEmail, invalidCountry)}");
-            Console.WriteLine($"isValid: {manager.FullValidation(validAmount, validEmail, validCountry)}");
+            isValid = manager.FullValidation(validAmount, validEmail, validCountry);
+            Console.WriteLine($"Basic.ValidatorManager.FullValidation() => {isValid}");
 
             Console.WriteLine();
             Console.WriteLine("===================================================");
             Console.WriteLine();
-
         }
 
         static void SimpleDemo()
         {
             var manager = new Simple.ValidatorManager();
+            bool isValid;
 
-            Console.WriteLine($"Simple.ValidatorManager.BasicValidation() => ");
-            Console.WriteLine($"isValid: {manager.BasicValidation(new Simple.Validators.ValidateContext { Amount = invalidAmount, Email = validEmail })}");
-            Console.WriteLine($"isValid: {manager.BasicValidation(new Simple.Validators.ValidateContext { Amount = validAmount, Email = invalidEmail })}");
-            Console.WriteLine($"isValid: {manager.BasicValidation(new Simple.Validators.ValidateContext { Amount = validAmount, Email = validEmail })}");
+            isValid = manager.BasicValidation(new Simple.Validators.ValidateContext { Amount = validAmount, Email = validEmail });
+            Console.WriteLine($"Simple.ValidatorManager.BasicValidation() => {isValid}");
 
-            Console.WriteLine();
-
-            Console.WriteLine($"Simple.ValidatorManager.FullValidation() => ");
-            Console.WriteLine($"isValid: {manager.FullValidation(new Simple.Validators.ValidateContext { Amount = invalidAmount, Email = validEmail, Country = validCountry })}");
-            Console.WriteLine($"isValid: {manager.FullValidation(new Simple.Validators.ValidateContext { Amount = validAmount, Email = invalidEmail, Country = validCountry })}");
-            Console.WriteLine($"isValid: {manager.FullValidation(new Simple.Validators.ValidateContext { Amount = validAmount, Email = validEmail, Country = invalidCountry })}");
-            Console.WriteLine($"isValid: {manager.FullValidation(new Simple.Validators.ValidateContext { Amount = validAmount, Email = validEmail, Country = validCountry })}");
+            isValid = manager.FullValidation(new Simple.Validators.ValidateContext { Amount = validAmount, Email = validEmail, Country = validCountry });
+            Console.WriteLine($"Simple.ValidatorManager.FullValidation() => {isValid}");
 
             Console.WriteLine();
             Console.WriteLine("===================================================");
@@ -81,19 +68,13 @@ namespace ValidatorManager
         static void StandardDemo()
         {
             var manager = new Standard.ValidatorManager();
+            bool isValid;
 
-            Console.WriteLine($"Standard.ValidatorManager.BasicValidation() => ");
-            Console.WriteLine($"isValid: {manager.BasicValidation(new Standard.Validators.ValidateContext { Amount = invalidAmount, Email = validEmail })}");
-            Console.WriteLine($"isValid: {manager.BasicValidation(new Standard.Validators.ValidateContext { Amount = validAmount, Email = invalidEmail })}");
-            Console.WriteLine($"isValid: {manager.BasicValidation(new Standard.Validators.ValidateContext { Amount = validAmount, Email = validEmail })}");
+            isValid = manager.BasicValidation(new Standard.Validators.ValidateContext { Amount = validAmount, Email = validEmail });
+            Console.WriteLine($"Standard.ValidatorManager.BasicValidation() => {isValid}");
 
-            Console.WriteLine();
-
-            Console.WriteLine($"Standard.ValidatorManager.FullValidation() => ");
-            Console.WriteLine($"isValid: {manager.FullValidation(new Standard.Validators.ValidateContext { Amount = invalidAmount, Email = validEmail, Country = validCountry })}");
-            Console.WriteLine($"isValid: {manager.FullValidation(new Standard.Validators.ValidateContext { Amount = validAmount, Email = invalidEmail, Country = validCountry })}");
-            Console.WriteLine($"isValid: {manager.FullValidation(new Standard.Validators.ValidateContext { Amount = validAmount, Email = validEmail, Country = invalidCountry })}");
-            Console.WriteLine($"isValid: {manager.FullValidation(new Standard.Validators.ValidateContext { Amount = validAmount, Email = validEmail, Country = validCountry })}");
+            isValid = manager.FullValidation(new Standard.Validators.ValidateContext { Amount = validAmount, Email = validEmail, Country = validCountry });
+            Console.WriteLine($"Standard.ValidatorManager.FullValidation() => {isValid}");
 
             Console.WriteLine();
             Console.WriteLine("===================================================");
@@ -103,19 +84,13 @@ namespace ValidatorManager
         static void AdvanceDemo()
         {
             var manager = new Advance.ValidatorManager();
+            bool isValid;
 
-            Console.WriteLine($"Advance.ValidatorManager.BasicValidation() => ");
-            Console.WriteLine($"isValid: {manager.BasicValidation(new Advance.Validators.ValidateContext { Amount = invalidAmount, Email = validEmail })}");
-            Console.WriteLine($"isValid: {manager.BasicValidation(new Advance.Validators.ValidateContext { Amount = validAmount, Email = invalidEmail })}");
-            Console.WriteLine($"isValid: {manager.BasicValidation(new Advance.Validators.ValidateContext { Amount = validAmount, Email = validEmail })}");
+            isValid = manager.BasicValidation(new Advance.Validators.ValidateContext { Amount = validAmount, Email = validEmail });
+            Console.WriteLine($"Advance.ValidatorManager.BasicValidation() => {isValid}");
 
-            Console.WriteLine();
-
-            Console.WriteLine($"Advance.ValidatorManager.FullValidation() => ");
-            Console.WriteLine($"isValid: {manager.FullValidation(new Advance.Validators.ValidateContext { Amount = invalidAmount, Email = validEmail, Country = validCountry })}");
-            Console.WriteLine($"isValid: {manager.FullValidation(new Advance.Validators.ValidateContext { Amount = validAmount, Email = invalidEmail, Country = validCountry })}");
-            Console.WriteLine($"isValid: {manager.FullValidation(new Advance.Validators.ValidateContext { Amount = validAmount, Email = validEmail, Country = invalidCountry })}");
-            Console.WriteLine($"isValid: {manager.FullValidation(new Advance.Validators.ValidateContext { Amount = validAmount, Email = validEmail, Country = validCountry })}");
+            isValid = manager.FullValidation(new Advance.Validators.ValidateContext { Amount = validAmount, Email = validEmail, Country = validCountry });
+            Console.WriteLine($"Advance.ValidatorManager.FullValidation() => {isValid}");
 
             Console.WriteLine();
             Console.WriteLine("===================================================");
@@ -125,36 +100,16 @@ namespace ValidatorManager
         static void ComplexDemo()
         {
             var manager = new Complex.ValidatorManager();
+            bool isValid;
 
-            Console.WriteLine("Complex.ValidatorManager.BasicValidation() => ");
-            Console.WriteLine("isValid: " +
-                                manager.BasicValidation(new Complex.ChainValidators.ValidateContext { Amount = invalidAmount, Email = validEmail },
-                                                        new Complex.IndividualValidators.ValidateContext() { Foo = validFoo }));
-            Console.WriteLine("isValid: " +
-                                manager.BasicValidation(new Complex.ChainValidators.ValidateContext { Amount = validAmount, Email = invalidEmail },
-                                                        new Complex.IndividualValidators.ValidateContext() { Foo = validFoo }));
-            Console.WriteLine("isValid: " +
-                                manager.BasicValidation(new Complex.ChainValidators.ValidateContext { Amount = validAmount, Email = validEmail },
-                                                        new Complex.IndividualValidators.ValidateContext() { Foo = inValidFoo }));
-            Console.WriteLine("isValid: " +
-                                manager.BasicValidation(new Complex.ChainValidators.ValidateContext { Amount = invalidAmount, Email = invalidEmail },
-                                                        new Complex.IndividualValidators.ValidateContext() { Foo = validFoo }));
-            Console.WriteLine("isValid: " +
-                                manager.BasicValidation(new Complex.ChainValidators.ValidateContext { Amount = invalidAmount, Email = validEmail },
-                                                        new Complex.IndividualValidators.ValidateContext() { Foo = inValidFoo }));
-            Console.WriteLine("isValid: " +
-                                manager.BasicValidation(new Complex.ChainValidators.ValidateContext { Amount = validAmount, Email = invalidEmail },
-                                                        new Complex.IndividualValidators.ValidateContext() { Foo = inValidFoo }));
-            Console.WriteLine("isValid: " +
-                                manager.BasicValidation(new Complex.ChainValidators.ValidateContext { Amount = invalidAmount, Email = invalidEmail },
-                                                        new Complex.IndividualValidators.ValidateContext() { Foo = inValidFoo }));
-            Console.WriteLine("isValid: " +
-                                manager.BasicValidation(new Complex.ChainValidators.ValidateContext { Amount = validAmount, Email = validEmail },
-                                                        new Complex.IndividualValidators.ValidateContext() { Foo = validFoo }));
+            isValid = manager.BasicValidation(new Complex.ChainValidators.ValidateContext { Amount = validAmount, Email = validEmail },
+                                              new Complex.IndividualValidators.ValidateContext() { Foo = validFoo });
+            Console.WriteLine($"Complex.ValidatorManager.BasicValidation() => {isValid}");
 
-            Console.WriteLine();
+            isValid = manager.FullValidation(new Complex.ChainValidators.ValidateContext { Amount = validAmount, Email = validEmail, Country = validCountry },
+                                             new Complex.IndividualValidators.ValidateContext() { Foo = validFoo, Bar = validBar });
+            Console.WriteLine($"Complex.ValidatorManager.FullValidation() => {isValid}");
 
-            // to much scanario, skip...
             Console.WriteLine();
             Console.WriteLine("===================================================");
             Console.WriteLine();
---
+++ ValidatorManager/Tests.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatorManager
+{
+    public class Tests
+    {
+        private decimal validAmount = 11;
+        private decimal invalidAmount = 10;
+
+        private string validEmail = "ron.sun@mailserver.com";
+        private string invalidEmail = "a";
+
+        private string validCountry = "Taiwan";
+        private string invalidCountry = "a";
+
+        private string validFoo = "Foo";
+        private string invalidFoo = "a";
+
+        private string validBar = "Bar";
+        private string invalidBar = "a";
+
+        #region BasicTest
+
+        public void BasicTest()
+        {
+            var manager = new Basic.ValidatorManager();
+
+            Console.WriteLine($"Basic.ValidatorManager.BasicValidation() => ");
+            BasicTest_BasicValidation(invalidAmount, validEmail, false);
+            BasicTest_BasicValidation(validAmount, invalidEmail, false);
+            BasicTest_BasicValidation(validAmount, validEmail, true);
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Basic.ValidatorManager.FullValidation() => ");
+            BasicTest_FullValidation(invalidAmount, validEmail, validCountry, false);
+            BasicTest_FullValidation(validAmount, invalidEmail, validCountry, false);
+            BasicTest_FullValidation(validAmount, validEmail, invalidCountry, false);
+            BasicTest_FullValidation(validAmount, validEmail, validCountry, true);
+
+            Console.WriteLine();
+            Console.WriteLine("===================================================");
+            Console.WriteLine();
+        }
+
+        private void BasicTest_BasicValidation(decimal amount, string email, bool expected)
+        {
+            var actual = new Basic.ValidatorManager().BasicValidation(amount, email);
+            Assert(expected, actual);
+        }
+
+        private void BasicTest_FullValidation(decimal amount, string email, string country, bool expected)
+        {
+            var actual = new Basic.ValidatorManager().FullValidation(amount, email, country);
+            Assert(expected, actual);
+        }
+
+        #endregion
+
+        #region SimpleTest
+
+        public void SimpleTest()
+        {
+            var manager = new Simple.ValidatorManager();
+
+            Console.WriteLine($"Simple.ValidatorManager.BasicValidation() => ");
+            SimpleTest_BasicValidation(invalidAmount, validEmail, false);
+            SimpleTest_BasicValidation(validAmount, invalidEmail, false);
+            SimpleTest_BasicValidation(validAmount, validEmail, true);
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Simple.ValidatorManager.FullValidation() => ");
+            SimpleTest_FullValidation(invalidAmount, validEmail, validCountry, false);
+            SimpleTest_FullValidation(validAmount, invalidEmail, validCountry, false);
+            SimpleTest_FullValidation(validAmount, validEmail, invalidCountry, false);
+            SimpleTest_FullValidation(validAmount, validEmail, validCountry, true);
+
+            Console.WriteLine();
+            Console.WriteLine("===================================================");
+            Console.WriteLine();
+        }
+
+        private void SimpleTest_BasicValidation(decimal amount, string email, bool expected)
+        {
+            var validationContext = new Simple.Validators.ValidateContext()
+            {
+                Amount = amount,
+                Email = email
+            };
+            var actual = new Simple.ValidatorManager().BasicValidation(validationContext);
+            Assert(expected, actual);
+        }
+
+        private void SimpleTest_FullValidation(decimal amount, string email, string country, bool expected)
+        {
+            var validationContext = new Simple.Validators.ValidateContext()
+            {
+                Amount = amount,
+                Email = email,
+                Country = country
+            };
+            var actual = new Simple.ValidatorManager().FullValidation(validationContext);
+            Assert(expected, actual);
+        }
+
+        #endregion
+
+        #region AdvanceTest
+
+        public void AdvanceTest()
+        {
+            var manager = new Advance.ValidatorManager();
+
+            Console.WriteLine($"Advance.ValidatorManager.BasicValidation() => ");
+            AdvanceTest_BasicValidation(invalidAmount, validEmail, false);
+            AdvanceTest_BasicValidation(validAmount, invalidEmail, false);
+            AdvanceTest_BasicValidation(validAmount, validEmail, true);
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Advance.ValidatorManager.FullValidation() => ");
+            AdvanceTest_FullValidation(invalidAmount, validEmail, validCountry, false);
+            AdvanceTest_FullValidation(validAmount, invalidEmail, validCountry, false);
+            AdvanceTest_FullValidation(validAmount, validEmail, invalidCountry, false);
+            AdvanceTest_FullValidation(validAmount, validEmail, validCountry, true);
+
+            Console.WriteLine();
+            Console.WriteLine("===================================================");
+            Console.WriteLine();
+        }
+
+        private void AdvanceTest_BasicValidation(decimal amount, string email, bool expected)
+        {
+            var validationContext = new Advance.Validators.ValidateContext()
+            {
+                Amount = amount,
+                Email = email
+            };
+            var actual = new Advance.ValidatorManager().BasicValidation(validationContext);
+            Assert(expected, actual);
+        }
+
+        private void AdvanceTest_FullValidation(decimal amount, string email, string country, bool expected)
+        {
+            var validationContext = new Advance.Validators.ValidateContext()
+            {
+                Amount = amount,
+                Email = email,
+                Country = country
+            };
+            var actual = new Advance.ValidatorManager().FullValidation(validationContext);
+            Assert(expected, actual);
+        }
+
+        #endregion
+
+        #region ComplexTest
+
+        public void ComplexTest()
+        {
+            var manager = new Complex.ValidatorManager();
+
+            Console.WriteLine($"Complex.ValidatorManager.BasicValidation() => ");
+            ComplexTest_BasicValidation(invalidAmount, validEmail, validFoo, false);
+            ComplexTest_BasicValidation(validAmount, invalidEmail, validFoo, false);
+            ComplexTest_BasicValidation(validAmount, validEmail, invalidFoo, false);
+
+            ComplexTest_BasicValidation(invalidAmount, invalidEmail, validFoo, false);
+            ComplexTest_BasicValidation(invalidAmount, validEmail, invalidFoo, false);
+            ComplexTest_BasicValidation(validAmount, invalidEmail, invalidFoo, false);
+
+            ComplexTest_BasicValidation(invalidAmount, invalidEmail, invalidFoo, false);
+
+            ComplexTest_BasicValidation(validAmount, validEmail, validFoo, true);
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Complex.ValidatorManager.FullValidation() => ");
+
+            // 1 invalid parameter
+            ComplexTest_FullValidation(invalidAmount, validEmail, validCountry, validFoo, validBar, false);
+            ComplexTest_FullValidation(validAmount, invalidEmail, validCountry, validFoo, validBar, false);
+            ComplexTest_FullValidation(validAmount, validEmail, invalidCountry, validFoo, validBar, false);
+            ComplexTest_FullValidation(validAmount, validEmail, validCountry, invalidFoo, validBar, false);
+            ComplexTest_FullValidation(validAmount, validEmail, validCountry, validFoo, invalidBar, false);
+
+            // 2 invalid parameters
+            ComplexTest_FullValidation(invalidAmount, invalidEmail, validCountry, validFoo, validBar, false);
+            ComplexTest_FullValidation(invalidAmount, validEmail, invalidCountry, validFoo, validBar, false);
+            ComplexTest_FullValidation(invalidAmount, validEmail, validCountry, invalidFoo, validBar, false);
+            ComplexTest_FullValidation(invalidAmount, validEmail, validCountry, validFoo, invalidBar, false);
+
+            ComplexTest_FullValidation(validAmount, invalidEmail, invalidCountry, validFoo, validBar, false);
+            ComplexTest_FullValidation(validAmount, invalidEmail, validCountry, invalidFoo, validBar, false);
+            ComplexTest_FullValidation(validAmount, invalidEmail, validCountry, validFoo, invalidBar, false);
+
+            ComplexTest_FullValidation(validAmount, validEmail, invalidCountry, invalidFoo, validBar, false);
+            ComplexTest_FullValidation(validAmount, validEmail, invalidCountry, validFoo, invalidBar, false);
+
+            ComplexTest_FullValidation(validAmount, validEmail, validCountry, invalidFoo, invalidBar, false);
+
+            // 3 invalid parameters (2 valid parameters)
+            ComplexTest_FullValidation(validAmount, validEmail, invalidCountry, invalidFoo, invalidBar, false);
+            ComplexTest_FullValidation(validAmount, invalidEmail, validCountry, invalidFoo, invalidBar, false);
+            ComplexTest_FullValidation(validAmount, invalidEmail, invalidCountry, validFoo, invalidBar, false);
+            ComplexTest_FullValidation(validAmount, invalidEmail, invalidCountry, invalidFoo, validBar, false);
+
+            ComplexTest_FullValidation(invalidAmount, validEmail, validCountry, invalidFoo, invalidBar, false);
+            ComplexTest_FullValidation(invalidAmount, validEmail, invalidCountry, validFoo, invalidBar, false);
+            ComplexTest_FullValidation(invalidAmount, validEmail, invalidCountry, invalidFoo, validBar, false);
+
+            ComplexTest_FullValidation(invalidAmount, invalidEmail, validCountry, validFoo, invalidBar, false);
+            ComplexTest_FullValidation(invalidAmount, invalidEmail, validCountry, invalidFoo, validBar, false);
+
+            ComplexTest_FullValidation(invalidAmount, invalidEmail, invalidCountry, validFoo, validBar, false);
+
+            // 4 invalid parameters (1 valid parameters)
+            ComplexTest_FullValidation(validAmount, invalidEmail, invalidCountry, invalidFoo, invalidBar, false);
+            ComplexTest_FullValidation(invalidAmount, validEmail, invalidCountry, invalidFoo, invalidBar, false);
+            ComplexTest_FullValidation(invalidAmount, invalidEmail, validCountry, invalidFoo, invalidBar, false);
+            ComplexTest_FullValidation(invalidAmount, invalidEmail, invalidCountry, validFoo, invalidBar, false);
+            ComplexTest_FullValidation(invalidAmount, invalidEmail, invalidCountry, invalidFoo, validBar, false);
+
+            // all invalid parameters
+            ComplexTest_FullValidation(invalidAmount, invalidEmail, invalidCountry, invalidFoo, invalidBar, false);
+
+            // all valid parameters
+            ComplexTest_FullValidation(validAmount, validEmail, validCountry, validFoo, validBar, true);
+
+            Console.WriteLine();
+            Console.WriteLine("===================================================");
+            Console.WriteLine();
+        }
+
+        private void ComplexTest_BasicValidation(decimal amount,
+                                                 string email,
+                                                 string foo,
+                                                 bool expected)
+        {
+            var chainValidationContext = new Complex.ChainValidators.ValidateContext()
+            {
+                Amount = amount,
+                Email = email
+            };
+
+            var individualValidationContext = new Complex.IndividualValidators.ValidateContext()
+            {
+                Foo = foo
+            };
+            var actual = new Complex.ValidatorManager().BasicValidation(chainValidationContext, individualValidationContext);
+            Assert(expected, actual);
+        }
+
+        private void ComplexTest_FullValidation(decimal amount,
+                                                string email,
+                                                string country,
+                                                string foo,
+                                                string bar,
+                                                bool expected)
+        {
+            var chainValidationContext = new Complex.ChainValidators.ValidateContext()
+            {
+                Amount = amount,
+                Email = email,
+                Country = country
+            };
+
+            var individualValidationContext = new Complex.IndividualValidators.ValidateContext()
+            {
+                Foo = foo,
+                Bar = bar
+            };
+            var actual = new Complex.ValidatorManager().FullValidation(chainValidationContext, individualValidationContext);
+            Assert(expected, actual);
+        }
+
+        #endregion
+
+        private void Assert(bool expected, bool actual)
+        {
+            if (expected != actual)
+            {
+                throw new Exception("actual not match expected");
+            }
+
+            Console.WriteLine($"expected: {expected}, actual match expected");
+        }
+    }
+}
